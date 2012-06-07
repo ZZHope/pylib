ffa3ea208418ff04ae7f85ea904e3a00ba2830ae
fherwig <fherwig@2e4cce12-393c-0410-8681-c83ea65bbeac>
2012-06-07 22:40:13 +0000

    deleted commented routines in mppnp.py
    git-svn-id: svn://forum.astro.keele.ac.uk/utils/pylib@2560 2e4cce12-393c-0410-8681-c83ea65bbeac

--- utils.py
+++ utils.py
@@ -165,7 +165,8 @@ class Utils():
 	    'Cu','Zn','Ga','Ge','As','Se','Br','Kr','Rb','Sr','Y','Zr','Nb','Mo','Tc',
 	    'Ru','Rh','Pd','Ag','Cd','In','Sn','Sb','Te', 'I','Xe','Cs','Ba','La','Ce',
 	    'Pr','Nd','Pm','Sm','Eu','Gd','Tb','Dy','Ho','Er','Tm','Yb','Lu','Hf','Ta',
-	    'W','Re','Os','Ir','Pt','Au','Hg','Tl','Pb','Bi','Po','At','Rn','Fr','Ra','Ac','Th','Pa','U','Np','Pu']
+	    'W','Re','Os','Ir','Pt','Au','Hg','Tl','Pb','Bi','Po','At','Rn','Fr','Ra','Ac','Th',
+	    'Pa','U','Np','Pu']
 
     	stable_el = [['Neutron','999'],['H',1, 2],['He', 3, 4],['Li', 6, 7],['Be', 9],
 		     ['B', 10, 11],['C', 12, 13],['N', 14, 15],['O', 16, 17, 18],['F', 19],
